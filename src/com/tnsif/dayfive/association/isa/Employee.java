72c75ff445e7c20c3ea0badf4140d16d5c96194e
user name <ddeepikab2@gmail.coml>
2025-06-29 06:57:41 +0530

    day5

---
+++ src/com/tnsif/dayfive/association/isa/Employee.java
@@ -0,0 +1,22 @@
+package com.tnsif.dayfive.association.isa;
+
+public class Employee {
+	
+	private String name;
+	private int employeeId;
+	private String department;
+
+	// constructors
+	public Employee() {
+		super();
+	}
+
+	public Employee(String name, int employeeId, String department) {
+		super();
+		this.name = name;
+		this.employeeId = employeeId;
+		this.department = department;
+		
+	}
+
+}
