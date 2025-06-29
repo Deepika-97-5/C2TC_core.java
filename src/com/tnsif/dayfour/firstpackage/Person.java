9800de8789d591fcd2e221fbc5bb2ad4802467ff
user name <ddeepikab2@gmail.coml>
2025-06-29 06:26:33 +0530

    day4

---
+++ src/com/tnsif/dayfour/firstpackage/Person.java
@@ -0,0 +1,22 @@
+package com.tnsif.dayfour.firstpackage;
+
+public class Person {
+	
+	private String personName;
+	private int personAge;
+	private String personCity;
+	
+	public String getPersonName() {
+		return personName;
+	}
+
+	public void setPersonName(String personName) {
+		this.personName = personName;
+	}
+
+	public int getPersonAge() {
+		return personAge;
+	}
+
+
+}
