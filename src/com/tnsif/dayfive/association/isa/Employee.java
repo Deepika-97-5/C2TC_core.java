104e665f4645263e0c9d8df6a50e2cdaae5e711b
user name <ddeepikab2@gmail.coml>
2025-07-04 10:09:47 +0530

    day5

--- src/com/tnsif/dayfive/association/isa/Employee.java
+++ src/com/tnsif/dayfive/association/isa/Employee.java
@@ -1,7 +1,8 @@
+//Program to demonstrate usage of IS A Relationship 
 package com.tnsif.dayfive.association.isa;
 
+// Superclass
 public class Employee {
-	
 	private String name;
 	private int employeeId;
 	private String department;
@@ -16,7 +17,37 @@ public class Employee {
 		this.name = name;
 		this.employeeId = employeeId;
 		this.department = department;
-		
+
+	}
+
+	// getter/setter methods
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [Name=" + name + ", EmployeeId=" + employeeId + ", Department=" + department + "]";
 	}
 
 }
